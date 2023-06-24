9f2f69822600a06578c5368c73e90922f70f46cd
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-06-24 15:59:50 +0530

    Remove unnecessary code

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use lazy_static::lazy_static;
 use regex::Regex;
 use reqwest::IntoUrl;
 use scraper::{Html, Selector};
-use ttf_parser::OutlineBuilder;
 
 lazy_static! {
     static ref FONT_FACE_REGEX: Regex = Regex::new(r"@font-face\s*\{([^}]+)\}").unwrap();
@@ -42,34 +41,6 @@ pub struct ScrapedCss {
     pub fonts: Vec<ScrapedFileRaw>,
     // (Absolute url, file name)
     pub imports: Vec<(String, String)>,
-}
-
-struct Builder<'a>(&'a mut String);
-
-impl OutlineBuilder for Builder<'_> {
-    fn move_to(&mut self, x: f32, y: f32) {
-        use std::fmt::Write;
-        write!(self.0, "M {} {} ", x, y).unwrap()
-    }
-
-    fn line_to(&mut self, x: f32, y: f32) {
-        use std::fmt::Write;
-        write!(self.0, "L {} {} ", x, y).unwrap()
-    }
-
-    fn quad_to(&mut self, x1: f32, y1: f32, x: f32, y: f32) {
-        use std::fmt::Write;
-        write!(self.0, "Q {} {} {} {} ", x1, y1, x, y).unwrap()
-    }
-
-    fn curve_to(&mut self, x1: f32, y1: f32, x2: f32, y2: f32, x: f32, y: f32) {
-        use std::fmt::Write;
-        write!(self.0, "C {} {} {} {} {} {} ", x1, y1, x2, y2, x, y).unwrap()
-    }
-
-    fn close(&mut self) {
-        self.0.push_str("Z ")
-    }
 }
 
 pub async fn scrap_css<T: IntoUrl>(base: T) -> Result<ScrapedCss, Box<dyn std::error::Error>> {
