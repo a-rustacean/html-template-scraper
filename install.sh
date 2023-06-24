b162512de3d5d7a57e33b7d9f9bb5158fd92f524
a-rustacean <dilshadplayingminecraft@outlook.com>
2023-06-24 15:48:59 +0530

    Update `install.sh`

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 cargo build --release
 rm -rf $HOME/.local/bin/scrap-html
-ln -s $(pwd)/target/release/html-template-scrapper $HOME/.local/bin/scrap-html
+ln -s $(pwd)/target/release/html-template-scraper $HOME/.local/bin/scrap-html
 chmod u+x $HOME/.local/bin/scrap-html
